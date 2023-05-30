1ec4d7e694d6270a388f085e2ecd47c7c1306209
HeoSeongMun <100749666+HeoSeongMun@users.noreply.github.com>
2023-05-31 03:12:05 +0900

    1
    1

--- flutter_app/lib/login.dart
+++ flutter_app/lib/login.dart
@@ -57,54 +57,87 @@ class Login extends StatelessWidget {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 85),
-              child: Text(
-                '휴게소에서 뭐먹지?',
-                style: TextStyle(
-                  fontFamily: 'pretendard',
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
+      body: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 85),
+                child: Text(
+                  '휴게소에서 뭐먹지?',
+                  style: TextStyle(
+                    fontFamily: 'pretendard',
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 60, bottom: 20),
-              child: Image.asset(
-                'assets/images/appicon.png',
-                height: 230,
-                width: 250,
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 60, bottom: 20),
+                child: Image.asset(
+                  'assets/images/appicon.png',
+                  height: 230,
+                  width: 250,
+                ),
               ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(
-                top: 30,
-              ),
-              height: 400,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40)),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    child: Container(
-                      alignment: Alignment(0.0, 0.0),
-                      height: 45,
-                      margin: EdgeInsets.only(left: 30, right: 30, top: 15),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(
+                  top: 30,
+                ),
+                height: 355,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
                       child: Container(
-                        child: TextField(
-                          controller: emailController,
-                          textInputAction: TextInputAction.next,
+                        alignment: Alignment(0.0, 0.0),
+                        height: 45,
+                        margin: EdgeInsets.only(left: 30, right: 30, top: 15),
+                        child: Container(
+                          child: TextField(
+                            controller: emailController,
+                            textInputAction: TextInputAction.next,
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                isDense: true,
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.black, width: 2),
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.black, width: 2),
+                                ),
+                                hintText: 'E-mail',
+                                hintStyle: TextStyle(
+                                    color: Colors.grey[300],
+                                    fontStyle: FontStyle.italic)),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Container(
+                        alignment: Alignment(0.0, 0.0),
+                        height: 45,
+                        margin: EdgeInsets.only(
+                            left: 30, right: 30, top: 30, bottom: 50),
+                        child: Container(
+                            child: TextField(
+                          controller: pwController,
                           style: TextStyle(color: Colors.black),
                           decoration: InputDecoration(
                               isDense: true,
@@ -116,152 +149,124 @@ class Login extends StatelessWidget {
                                 borderSide:
                                     BorderSide(color: Colors.black, width: 2),
                               ),
-                              hintText: 'E-mail',
+                              hintText: 'Password',
                               hintStyle: TextStyle(
                                   color: Colors.grey[300],
                                   fontStyle: FontStyle.italic)),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        )),
                       ),
                     ),
-                  ),
-                  Container(
-                    child: Container(
-                      alignment: Alignment(0.0, 0.0),
-                      height: 45,
-                      margin: EdgeInsets.only(
-                          left: 30, right: 30, top: 30, bottom: 50),
-                      child: Container(
-                          child: TextField(
-                        controller: pwController,
-                        style: TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                            isDense: true,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 2),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 2),
-                            ),
-                            hintText: 'Password',
-                            hintStyle: TextStyle(
-                                color: Colors.grey[300],
-                                fontStyle: FontStyle.italic)),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      )),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        final newUser =
-                            await _authentication.signInWithEmailAndPassword(
-                          email: emailController.text,
-                          password: pwController.text,
-                        );
-                        if (newUser.user != null) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const Home();
-                              },
+                    ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          final newUser =
+                              await _authentication.signInWithEmailAndPassword(
+                            email: emailController.text,
+                            password: pwController.text,
+                          );
+                          if (newUser.user != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const Home();
+                                },
+                              ),
+                            );
+                          }
+                        } catch (e) {
+                          print(e);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('이메일과 비밀번호를 확인해주세요'),
+                              backgroundColor: Colors.blue,
                             ),
                           );
                         }
-                      } catch (e) {
-                        print(e);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('이메일과 비밀번호를 확인해주세요'),
-                            backgroundColor: Colors.blue,
-                          ),
-                        );
-                      }
-                      /*final String id = idController.text;
-                            final String pw = pwController.text;*/
-                    },
-                    style: TextButton.styleFrom(
-                        primary: Colors.black,
-                        backgroundColor: Color(0xFFB1B2FF),
-                        padding: EdgeInsets.only(
-                            left: 30, right: 30, top: 8, bottom: 8),
-                        shadowColor: Colors.black,
-                        elevation: 7,
-                        side: BorderSide(
-                          color: Colors.black,
-                          width: 1.5,
-                        )),
-                    child: Text(
-                      "로그인",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                        /*final String id = idController.text;
+                              final String pw = pwController.text;*/
+                      },
+                      style: TextButton.styleFrom(
+                          primary: Colors.black,
+                          backgroundColor: Color(0xFFB1B2FF),
+                          padding: EdgeInsets.only(
+                              left: 30, right: 30, top: 8, bottom: 8),
+                          shadowColor: Colors.black,
+                          elevation: 7,
+                          side: BorderSide(
+                            color: Colors.black,
+                            width: 1.5,
+                          )),
+                      child: Text(
+                        "로그인",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                      child: Container(
-                          margin: EdgeInsets.only(top: 50),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  primary: Colors.black,
-                                ),
-                                child: Text(
-                                  '아이디 찾기',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
+                    Flexible(
+                        child: Container(
+                            margin: EdgeInsets.only(top: 50),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                TextButton(
+                                  onPressed: () {},
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.black,
+                                  ),
+                                  child: Text(
+                                    '아이디 찾기',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Text(' | '),
-                              TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  primary: Colors.black,
-                                ),
-                                child: Text(
-                                  '비밀번호 찾기',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
+                                Text(' | '),
+                                TextButton(
+                                  onPressed: () {},
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.black,
+                                  ),
+                                  child: Text(
+                                    '비밀번호 찾기',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Text(' | '),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Signup()),
-                                  );
-                                },
-                                style: TextButton.styleFrom(
-                                  primary: Colors.black,
-                                ),
-                                child: Text(
-                                  '회원가입',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
+                                Text(' | '),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Signup()),
+                                    );
+                                  },
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.black,
+                                  ),
+                                  child: Text(
+                                    '회원가입',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          )))
-                ],
+                              ],
+                            )))
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
